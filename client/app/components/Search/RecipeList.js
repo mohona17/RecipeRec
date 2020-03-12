b96be22e190e919cbacf0ed20cad0793ae902bec
mohona17 <mohonaahmed@ufl.edu>
2020-03-11 23:17:35 -0400

    basic card layout with summary

--- client/app/components/Search/RecipeList.js
+++ client/app/components/Search/RecipeList.js
@@ -1,4 +1,9 @@
 import React from 'react';
+const color1 = "#E8B866" //yellow
+const color2 = "#040A0F"//black
+const color3 = "#BD632F" //brown
+const color4 = "#D5D6D4" //lightgrey
+const color5 = "#B7B7B7" //darkgrey
 
 class RecipeList extends React.Component {
     constructor(props) {
@@ -31,12 +36,13 @@ class RecipeList extends React.Component {
     // }
     render() {
         const recipeDisplay = this.props.recipes.map((recipe, index) => {
-            // console.log(recipe)
-            // console.log(recipe.summary);
             return (
-                <div><h6>{recipe.title}</h6>
-                    <img src={recipe.image}></img>
-                    <p>{recipe.summary}</p>
+                <div style = {{backgroundColor:color5, borderRadius: '0.5rem', padding: '1rem', margin: '1rem'}}>
+                    <h2>{recipe.title}</h2>
+                    <div class = "row">
+                    <div class="col col-sm-4"><img src={recipe.image} width="300px"></img></div>
+                    <div class="col col-sm-8"><p>{recipe.summary}</p></div>
+                    </div>
                 </div>
             )
         });
